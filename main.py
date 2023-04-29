9b2c92581162ea0fa1b90534c750461b3244a339
Baehr3 <andy.huang12345@outlook.com>
2023-04-29 18:44:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-import requests
-from twilio.rest import Client
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-# Set environment variables for your credentials
-account_sid = "INSERT TWILIO ACCOUNT SID HERE"
-auth_token = "INSERT TOKEN HERE"
-client = Client(account_sid, auth_token)
 
-# client.messages.create(body, from, to), body is the message, from is the twilio number, to is where we send to
+from wolfram import query_wolfram
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
-    """Respond to incoming messages with a friendly SMS."""
-    # Get the incoming message
+    """Respond to incoming messages with a WolframAlpha API query response."""
     incoming_msg = request.values.get('Body', '')
-
-    # Create a Twilio response object
     resp = MessagingResponse()
 
+    load_dotenv()
+    app_id = os.getenv('WOLFRAM_APP_ID')
 
+    resp.message(query_wolfram(app_id, incoming_msg))
 
-# incoming_msg is what is received, pass into wolfram and pass the response into the thing below
-    resp.message(wolfram(input(incoming_msg)))
     return str(resp)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
