ccd068de15409d764b452d5f2becd773016af16e
tranq88 <50079142+tranq88@users.noreply.github.com>
2023-04-30 01:51:26 -0400

    handle case where wolfram doesn't understand

--- wolfram.py
+++ wolfram.py
@@ -7,17 +7,20 @@ def query_wolfram(app_id: str, query: str, user_num: str) -> str:
     Query the WolframAlpha Conversational API with key <app_id> and
     return the resulting response.
     """
-    with open('conversations.json', 'r+') as f:
-        convos = json.loads(f)  # load the json file into a dict
+    with open('conversations.json', 'r') as f:
+        convos = json.load(f)  # load the json file into a dict
 
+    try:
         # if the number already has an existing conversation
         if user_num in convos:
             conv_id = convos[user_num]['conversationID']
             host = convos[user_num]['host']
             s = convos[user_num]['s']
 
-            url = f'http://{host}/api/v1/conversation.jsp?appid={app_id}\
-                conversationid={conv_id}&i={query}'
+            url = (
+                f'http://{host}/api/v1/conversation.jsp?appid={app_id}&'
+                f'conversationid={conv_id}&i={query}'
+            )
             if s:
                 url += f'&s={s}'
 
@@ -32,20 +35,31 @@ def query_wolfram(app_id: str, query: str, user_num: str) -> str:
             else:
                 convos[user_num]['s'] = None
 
-            json.dump(convos, f)
+            with open('conversations.json', 'w') as f:
+                json.dump(convos, f, indent=4)
 
             return result['result']
 
         # first-time users
-        url = f'http://api.wolframalpha.com/v1/conversation.jsp\
-            ?appid={app_id}&i={query}'
+        url = (
+            f'http://api.wolframalpha.com/v1/conversation.jsp?'
+            f'appid={app_id}&i={query}'
+        )
         r = requests.get(url)
         result = r.json()
 
         if 's' not in result:
             result['s'] = None
 
-        convos[user_num] = result  # create new user entry
-        json.dump(convos, f)
+        # create new user entry
+        # we don't care about storing the result
+        ret = result['result']
+        result.pop('result')
+        convos[user_num] = result
 
-        return result['result']
+        with open('conversations.json', 'w') as f:
+            json.dump(convos, f, indent=4)
+
+        return ret
+    except KeyError:
+        return "Sorry, I don't understand."
